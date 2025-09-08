b34b4c7f30f02029d74ce8d3f59e6a5a866dacfc
Kunal Patil <kunal.patil.dev@gmail.com>
2025-09-08 16:28:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ import ViewLecture from "./pages/ViewLecture";
 import SearchWithAi from "./pages/SearchWithAi";
 import getAllReviews from "./customHooks/getAllReviews";
 
-export const serverUrl = "http://localhost:8000";
+export const serverUrl = "https://smartlms.onrender.com";
 
 function App() {
   let { userData } = useSelector((state) => state.user);
